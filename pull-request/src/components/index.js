82bac84a65626f0efdc2b60ea9a4f35d1013bc3c
“OGWJ” <owenglynwilliamjenkins@gmail.com>
2021-10-01 15:57:39 +0100

    stubbed message component, added MessengerPage

--- pull-request/src/components/index.js
+++ pull-request/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as BackButton } from './BackButton';
 export { default as PullRequestItem } from './PullRequestItem';
+export { default as Message } from './Message';
--- pull-request/src/pages/MessengerPage.js
+++ pull-request/src/pages/MessengerPage.js
@@ -1,5 +1,17 @@
+import React, { useState } from 'react';
+import { Message } from '../components';
+
 const MessengerPage = () => {
-    return <h1>TODO</h1>
+
+    const [ messages, setMessages ] = useState([]);
+
+    return (
+        <div className="container">
+            {
+                messages.map(message => <Message message={message}/>
+            }
+        </div>
+    )
 }
 
 export default MessengerPage;
