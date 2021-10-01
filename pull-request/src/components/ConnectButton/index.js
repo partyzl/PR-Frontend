cff12021bdcabebd4409e4e07cb5d239df7886ca
“ELOBO5” <elicialobo@hotmail.com>
2021-10-01 15:31:09 +0100

    create connect button

---
+++ pull-request/src/components/ConnectButton/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const ConnectButton = () => {
+  return (
+    <div>
+      <button className="connect-button">Connect</button>
+    </div>
+  );
+};
+
+export default ConnectButton;
