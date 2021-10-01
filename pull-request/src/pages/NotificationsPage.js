cb411886940d74310fcc7064bc0bf56ee73ae49a
“OGWJ” <owenglynwilliamjenkins@gmail.com>
2021-10-01 15:49:10 +0100

    added notification page containers

--- pull-request/src/pages/NotificationsPage.js
+++ pull-request/src/pages/NotificationsPage.js
@@ -1,5 +1,15 @@
 const NotificationsPage = () => {
-    return <h1>TODO</h1>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <ul className="w-100">
+                    // for each pr in context
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default NotificatonsPage;
